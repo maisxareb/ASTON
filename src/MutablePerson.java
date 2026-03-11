7edf1179d250982b2c6c3a1fef67e95685dc5f7a
Your Name <your.email@example.com>
2026-03-11 17:02:48 +0500

    Initial commit: Add immutable student classes

---
+++ src/MutablePerson.java
@@ -0,0 +1,35 @@
+class MutablePerson {
+    private String name;
+    private String secondName;
+    private int age;
+
+    public MutablePerson(String name, String secondName, int age) {
+        this.age = age;
+        this.name = name;
+        this.secondName = secondName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
